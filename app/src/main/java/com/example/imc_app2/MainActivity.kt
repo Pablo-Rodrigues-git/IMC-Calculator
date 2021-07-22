2f40213bbb5c67543148d3d54fbb8566c2e92b8a
Pablo Rodrigues <pablo.rodrigues2015@outlook.com>
2021-07-22 04:07:42 -0300

    Solved some tests and issue's.

--- app/src/main/java/com/example/imc_app2/MainActivity.kt
+++ app/src/main/java/com/example/imc_app2/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.imc_app2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
+
+    private fun setListeners() {
+        alturaEDT?.doAfterTextChanged { text ->
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+           // tittleTXT.text = text
+        }
+        CalcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            tittleTXT.text = "Seu IMC é:".format(imc)
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/imc_app2/ResultActivity.kt
+++ app/src/main/java/com/example/imc_app2/ResultActivity.kt
@@ -1,11 +1,74 @@
 package com.example.imc_app2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
-class ResultActivity : AppCompatActivity() {
+class Activity_main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(R.layout.activity_main)
+        setListeners()
+
+    }
+
+    private fun setListeners(){
+        alturaEDT?.doAfterTextChanged { text ->
+        }
+
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            // txtTitle?.text = text
+        }
+
+        CalcularBTN.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calcularIMC(peso:String, altura:String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if(peso!=null && altura!=null){
+            val imc = peso / (altura * altura)
+            var classificacao:String=""
+
+            when {
+                imc in 1.0..18.5 -> {
+                    classificacao = "🚨 MAGREZA"
+                    tittleTXT.text = "$classificacao\n" +
+                            "Atenção, seu nivel de IMC esta muito baixo"
+                    tittleTXT.text = "Seu IMC é $imc%2f".format((imc))
+                }
+                imc in 18.6..24.9 -> {
+                    classificacao = "👍 NORMAL"
+                    tittleTXT.text =
+                        "$classificacao\n😀 Parabens você esta no seu nivel ideal de IMC"
+                    tittleTXT.text = "Seu IMC é de $imc%2f".format((imc))
+                }
+                imc in 25.0..29.9 -> {
+                    classificacao = "⚠ SOBREPESO NIVEL 1"
+                    tittleTXT.text =
+                        "$classificacao\nAtenção, seu nivel de IMC esta levemente elevado"
+                    tittleTXT.text = "Seu IMC é de $imc%2f".format((imc))
+                }
+                imc in 30.0..39.9 -> {
+                    classificacao = "⚠ OBESIDADE NIVEL 2"
+                    tittleTXT.text = "$classificacao\n" +
+                            "Atenção, seu nivel de IMC esta acima do ideal"
+                    tittleTXT.text = "Seu IMC é de $imc%2f".format((imc))
+                }
+                imc > 40.0 -> {
+                    classificacao = "🚨 OBESIDADE GRAVE NIVEL 3"
+                    tittleTXT.text =
+                        "$classificacao\nMenos disso \uD83C\uDF54 \uD83C\uDF55 e mais disso \uD83D\uDEB4\u200D♂️\uD83C\uDFCB️\u200D♀"
+                    tittleTXT.text = "Seu IMC é de $imc%2f".format((imc))
+                }
+            }
+        }
     }
 }
